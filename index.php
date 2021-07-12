58f2c94e2f2eca5fba667b8fa57f9b91686fda0d
ather <Arthur.schimpf@gmx.de>
2021-07-12 02:09:33 +0200

    new base

---
+++ index.php
@@ -0,0 +1,75 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <script src="https://kit.fontawesome.com/ee02357a59.js" crossorigin="anonymous"></script>
+    <link rel="stylesheet" href=".goose/css/nav.css">
+    <link rel="stylesheet" href=".goose/css/filter-section.css">
+    <link rel="stylesheet" href=".goose/css/main.css">
+    <title>Gooseme</title>
+</head>
+<body>
+<!-- main-nav-bar -->
+<nav class="main-nav">
+    <div class="logo-section">
+        <img src=".goose/assets/images/logo/logo.png" alt="gooseme" class="logo">
+    </div>
+    <div class="searchbar-section">
+        <form class="searchbar" action="" method="">
+            <input class="search-input" type="text">
+            <button class="search-icon"><i class="fas fa-search"></i></button>
+        </form>
+    </div>
+    <div class="app-config-section">
+        <form class="config" action="" method="">
+            <button class="config-main"><i class="fas fa-moon"></i></button>
+            <button class="config-refresh"><i class="fas fa-sync-alt"></i></button>
+            <button class="config-main"><i class="fas fa-cog"></i></button>
+        </form>
+    </div>
+</nav>
+
+<!-- fav-filter-section -->
+
+<div class="fav-filter-section">
+    <div class="fav-start-icon"><i class="fas fa-star"></i></div>
+    <div class="fav-filter-item selected">goosemedemoproject</div>
+    <div class="fav-filter-item">gooseme</div>
+    <div class="fav-filter-item">beercraftshop</div>
+    <div class="fav-filter-item">eique.de</div>
+    <div class="fav-filter-item">bla</div>
+    <div class="fav-filter-item">blablabla</div>
+    <div class="fav-filter-item">someproject</div>
+    <div class="fav-filter-item">project</div>
+    <div class="fav-filter-item">githubproject</div>
+    <div class="fav-filter-item">testestest</div>
+</div>
+
+<!-- project filters -->
+<form class="project-filter-section">
+    <select>
+        <option value="" disabled="true" selected="selected">Type</option>
+        <option value="1">Git</option>
+        <option value="2">Symfony</option>
+        <option value="3">Laravel</option>
+        <option value="4">Laravel</option>
+    </select>
+    <select>
+        <option value="" disabled="true" selected="selected">Code</option>
+        <option value="1">Java</option>
+        <option value="2">Python</option>
+        <option value="2">JavaScript</option>
+        <option value="2">PHP</option>
+    </select>
+    <button class="reset-btn">reset</button>
+</form>
+<!-- project panel -->
+
+<!-- project selector section -->
+
+<!-- -->
+</body>
+</html>
